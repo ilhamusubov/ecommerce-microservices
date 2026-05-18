278e8a9fa66f51b01a505bc43c563e11315f40b4
ilhamusubov <ilham_0512@icloud.com>
2026-05-19 00:21:12 +0400

    Initial microservices structure

---
+++ api-gateway/src/test/java/com/ilham/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilham.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
